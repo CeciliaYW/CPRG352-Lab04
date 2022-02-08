5420cf4b208d6d37e302b755dc2358b044f1c0e9
ceciliayw <ceciliayw@YU.WANG02@EDU.SAIT.CA>
2022-02-07 19:30:49 -0700

    Lab 04 Frame only.

---
+++ src/java/MyLogin/AccountService.java
@@ -0,0 +1,21 @@
+package MyLogin;
+
+
+import models.User;
+
+
+public class AccountService {
+    
+    public User login(String username, String password){
+        
+        if((username.equals("abe")) || (username.equals("barb")) && password.equals("password")) {
+            return new User(username, null);
+        
+        }else {
+            return null;
+            
+        }
+    
+    }
+    
+}
